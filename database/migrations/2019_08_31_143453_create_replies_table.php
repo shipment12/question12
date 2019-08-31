3513cd2dc0653304ecad93c71d3e5a9d3f2ad0f5
udedibor favour <udedborfavour@gmail.com>
2019-08-31 23:07:07 +0100

    creating migration, model, controller

---
+++ database/migrations/2019_08_31_143453_create_replies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+
+            $table->integer('question_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('question_id')->reference('id')->on('questions')->onDelete('cascade');
+
+            $table->foreign('user_id')->reference('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
